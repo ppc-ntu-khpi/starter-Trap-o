f08024c9c16c5c98eb130a4a2a1774b90931df1a
Vitaliy <120330210+Trap-o@users.noreply.github.com>
2024-01-19 16:14:10 +0000

    gotovo

---
+++ src/Quotation.java
@@ -0,0 +1,7 @@
+public class Quotation {
+    String quote = "\nNow, as that ol' Greek dawg says, life's a banquet - so don't go thirsty, but don't get drunk, either.\n\t\t\t\t\t\t\tDexter DeShawn";
+
+    public void display() {
+        System.out.println(quote);
+    }
+}
